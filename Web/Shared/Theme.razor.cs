77ac8041de037acf3d2061e000e8b2318943d6b5
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-01 19:41:02 +0530

    add Theme Component

---
+++ Web/Shared/Theme.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Web.Models;
+
+namespace Web.Shared;
+
+public class ThemeBase : ComponentBase
+{
+    protected string Icon { get; set; } = "brightness-up";
+    protected bool MenuCollapsed { get; set; } = true;
+
+    protected void ToggleMenu()
+    {
+        MenuCollapsed = !MenuCollapsed;
+    }
+
+    protected void FocusOutHandler()
+    {
+        MenuCollapsed = true;
+    }
+
+    protected void SetTheme(UITheme theme) 
+    {
+        Icon = theme switch
+        {
+            UITheme.Light => "brightness-up",
+            UITheme.Dark => "moon",
+            _ => "device-desktop"
+        };
+    }
+}
