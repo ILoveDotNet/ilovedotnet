b767c96d53962729a1e3b5a39486e45f8d17a554
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-02 22:19:24 +0530

    Change bootstarp component back to #app and moved #blazor-error-ui back to index.html

--- Web/Program.cs
+++ Web/Program.cs
@@ -6,7 +6,7 @@ using Web;
 using Web.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#body");
+builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
