82fafb50c8eda9f550dc8abdc5377186893e9980
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-08 19:17:56 +0530

    Change setting dark class to html element from AppRoot Element

--- Web/Models/AppState.cs
+++ Web/Models/AppState.cs
@@ -2,16 +2,7 @@
 
 internal class AppState
 {
-    internal bool DarkMode { get; private set; }
-
     internal event Action OnChange;
 
     private void NotifyStateChanged() => OnChange?.Invoke();
-
-    internal void DisplayModeChanged(bool darkMode)
-    {
-        DarkMode = darkMode;
-
-        NotifyStateChanged();
-    }
 }
