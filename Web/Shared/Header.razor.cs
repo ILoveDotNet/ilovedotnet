d27e4fc121e7d7163a829b6132054e49bda09d35
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-08 22:02:53 +0530

    Header is now responsive

---
+++ Web/Shared/Header.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Web.Shared;
+
+public class HeaderBase : ComponentBase, IAsyncDisposable
+{
+    const ushort SMALLDEVICEWIDTH = 640;
+    
+    private IJSObjectReference? module;
+    private ushort viewPortWidth;
+
+    protected bool SmallDevice;
+    protected bool HideNonSearchItems;
+
+    [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/viewportsize.js");
+
+        viewPortWidth = await module.InvokeAsync<ushort>("getViewPortWidth");
+
+        SmallDevice = viewPortWidth < SMALLDEVICEWIDTH;
+    }
+
+    protected void Search() 
+    {
+        if (SmallDevice) 
+        {
+            HideNonSearchItems = true;
+        }
+    }
+
+    protected void ExitSearch()
+    {
+        if (!SmallDevice) return;
+        
+        HideNonSearchItems = false;
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (module is not null)
+        {
+            await module.DisposeAsync();
+        }
+    }
+}
