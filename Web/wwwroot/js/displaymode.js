e473cabccd64c90ba7f1fbb2ca002882f89c33e1
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-08 21:36:16 +0530

    Fix theme toggle not removing dark class

--- Web/wwwroot/js/displaymode.js
+++ Web/wwwroot/js/displaymode.js
@@ -1,8 +1,8 @@
 ﻿export function onDisplayModeChanged(mode) {
     if (mode == 'dark') {
-        document.documentElement.classList.add(mode);
+        document.documentElement.classList.add('dark');
     }
     else {
-        document.documentElement.classList.remove(mode);
+        document.documentElement.classList.remove('dark');
     }
 }
