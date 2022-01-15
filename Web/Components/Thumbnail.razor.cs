4dc993e119e4b85bca19a0d8270f4b6d2ca8e284
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-15 19:46:49 +0530

    Thumbnail improvements

--- Web/Components/Thumbnail.razor.cs
+++ Web/Components/Thumbnail.razor.cs
@@ -5,9 +5,10 @@ namespace Web.Components;
 public class ThumbnailBase : ComponentBase
 {
     [Parameter] public string Title { get; set; } = "Article title goes here and can span some line";
-    [Parameter] public string Url { get; set; } = "https://via.placeholder.com/285x160.webp";
+    [Parameter] public string ContentUrl { get; set; } = default!;
+    [Parameter] public string PosterUrl { get; set; } = "https://via.placeholder.com/285x160.webp";
     [Parameter] public string AltText { get; set; } = "banner";
-    [Parameter] public string Icon { get; set; } = "https://via.placeholder.com/48x48";
+    [Parameter] public string IconUrl { get; set; } = "https://via.placeholder.com/48x48";
     [Parameter] public string IconAlt { get; set; } = "stack logo";
     [Parameter] public DateOnly CreatedOn { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     [Parameter] public TimeOnly EstimatedTimeToRead { get; set; } = TimeOnly.FromDateTime(DateTime.Now);
