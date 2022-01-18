b1c81c47e071e1e5678141a9475cf8d8c7e04580
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-18 08:02:00 +0530

    Add GithubGistSnippet Component

---
+++ Web/Components/GithubGistSnippet.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Web.Components;
+
+public class GithubGistSnippetBase : ComponentBase, IAsyncDisposable
+{
+    private IJSObjectReference? module;
+
+    protected string Id = Guid.NewGuid().ToString();
+
+    [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+
+    [Parameter, EditorRequired] public string Title { get; set; } = default!;
+    [Parameter, EditorRequired] public string UserId { get; set; } = default!;
+    [Parameter, EditorRequired] public string FileName { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/githubgist.js");
+
+        await module.InvokeVoidAsync("printSnippetFrom", Id, UserId, FileName);
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (module is not null)
+        {
+            await module.DisposeAsync();
+        }
+    }
+}
---
+++ Web/wwwroot/js/githubgist.js
@@ -0,0 +1,20 @@
+﻿export function printSnippetFrom(id, userId, filename) {
+    const target = document.getElementById(id);
+    const iframe = document.createElement('iframe');
+    const iframeId = `${userId}-${filename}`;
+    iframe.setAttribute("id", iframeId);
+    iframe.setAttribute("width", "100%");
+    target.appendChild(iframe);
+
+    let doc = iframe.document;
+    if (iframe.contentDocument) doc = iframe.contentDocument;
+    else if (iframe.contentWindow) doc = iframe.contentWindow.document;
+
+    const gistScript = `<script src="https://gist.github.com/${userId}/${filename}.js"></script>`;
+    const resizeScript = `onload="parent.document.getElementById('${iframeId}').style.height=document.body.scrollHeight + 'px'"`;
+    const iframeHtml = `<html><body ${resizeScript}>${gistScript}</body></html>`;
+
+    doc.open();
+    doc.writeln(iframeHtml);
+    doc.close();
+}
