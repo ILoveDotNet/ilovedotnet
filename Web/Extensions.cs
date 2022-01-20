72d9a7a9c96d276c85be0c0475a4358456e856eb
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-21 00:40:24 +0530

    Add Fragment Navigation

---
+++ Web/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+public static class Extensions
+{
+    public static ValueTask NavigateToFragmentAsync(this NavigationManager navigationManager, IJSObjectReference? module)
+    {
+        var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+
+        if (uri.Fragment.Length == 0)
+        {
+            return default;
+        }
+
+        return module?.InvokeVoidAsync("scrollToFragment", uri.Fragment[1..]) ?? default;
+    }
+}
