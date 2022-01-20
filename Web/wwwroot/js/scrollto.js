b776a65c79cc6b4a12ad4ffbbff48b33066ee153
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-21 00:40:24 +0530

    Add Fragment Navigation

---
+++ Web/wwwroot/js/scrollto.js
@@ -0,0 +1,16 @@
+﻿export function scrollToFragment(elementId)
+{
+    var topoffset = document.getElementById('header').offsetHeight;
+    var element = document.getElementById(elementId);
+    if (element) {
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = element.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - topoffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    }
+}
