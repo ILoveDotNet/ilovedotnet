203eedf059e1454c8c089e0f3fa74036bd3c863e
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-24 01:20:52 +0530

    Commented service worker onFetch event handler to avoid cache issue

--- Web/wwwroot/service-worker.published.js
+++ Web/wwwroot/service-worker.published.js
@@ -4,7 +4,7 @@
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+//self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
