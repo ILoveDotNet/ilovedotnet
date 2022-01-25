9a8f5233e63e8882fc69d6b2089b55a6c8bf8044
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-26 00:15:24 +0530

    Add swap ui to thumbnail

--- Web/Components/Thumbnail.razor.cs
+++ Web/Components/Thumbnail.razor.cs
@@ -4,12 +4,13 @@ namespace Web.Components;
 
 public class ThumbnailBase : ComponentBase
 {
-    [Parameter] public string Title { get; set; } = "Article title goes here and can span some line";
-    [Parameter] public string ContentUrl { get; set; } = default!;
-    [Parameter] public string PosterUrl { get; set; } = "https://via.placeholder.com/285x160.webp";
-    [Parameter] public string AltText { get; set; } = "banner";
-    [Parameter] public string IconUrl { get; set; } = "https://via.placeholder.com/48x48";
-    [Parameter] public string IconAlt { get; set; } = "stack logo";
-    [Parameter] public DateTime CreatedOn { get; set; } = DateTime.Now;
-    [Parameter] public string Author { get; set; } = default!;
+    [Parameter, EditorRequired] public string Title { get; set; } = "Article title goes here and can span some line";
+    [Parameter, EditorRequired] public string ContentUrl { get; set; } = default!;
+    [Parameter, EditorRequired] public string PosterUrl { get; set; } = "https://via.placeholder.com/285x160.webp";
+    [Parameter, EditorRequired] public string AltText { get; set; } = "banner";
+    [Parameter, EditorRequired] public string IconUrl { get; set; } = "https://via.placeholder.com/48x48";
+    [Parameter, EditorRequired] public string IconAlt { get; set; } = "stack logo";
+    [Parameter, EditorRequired] public DateTime CreatedOn { get; set; } = DateTime.Now;
+    [Parameter, EditorRequired] public string Author { get; set; } = default!;
+    [Parameter] public bool Swap { get; set; } = default!;
 }
