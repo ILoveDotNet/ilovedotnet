c3c01897b51ab8297f2732d81e7a5f8315220485
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-29 00:47:23 +0530

    Add TableOfContents and ContentMetaData

---
+++ Web/Models/ContentMetaData.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Models;
+
+public class ContentMetaData
+{
+    public string Title { get; set; } = default!;
+    public string Author { get; set; } = default!;
+    public string PosterUrl { get; set; } = default!;
+    public string ThumbnailUrl { get; set; } = default!;
+    public string ContentUrl { get; set; } = default!;
+    public string IconUrl { get; set; } = default!;
+    public string Type { get; set; } = default!;
+    public DateTime CreatedOn { get; set; }
+}
