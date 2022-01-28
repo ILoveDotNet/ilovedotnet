d17aa6e45704af77ab418050e2c87c6f5420a3b9
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-29 00:47:23 +0530

    Add TableOfContents and ContentMetaData

---
+++ Web/Models/TableOfContents.cs
@@ -0,0 +1,42 @@
+﻿namespace Web.Models;
+
+public class TableOfContents
+{
+    public List<ContentMetaData> Contents { get; set; } =
+        new(3)
+        {
+            new ContentMetaData
+            {
+                Title = "Blazor WASM Exception Handling and Error Boundary",
+                Author = "Abdul Rahman",
+                PosterUrl = "image/blogs/blazor/wasm/blazor-wasm-exception-handling-and-error-boundary-1200w.svg",
+                ThumbnailUrl = "image/blogs/blazor/wasm/blazor-wasm-exception-handling-and-error-boundary-400w.svg",
+                ContentUrl = "blogs/blazor-wasm-exception-handling-and-error-boundary",
+                IconUrl = "image/icons/blazor.png",
+                Type = "Blazor",
+                CreatedOn = new DateTime(2022, 1, 16, 22, 30, 0)
+            },
+            new ContentMetaData
+            {
+                Title = "Blazor WASM Error Logging",
+                Author = "Abdul Rahman",
+                PosterUrl = "image/blogs/blazor/wasm/blazor-wasm-error-logging-1200w.svg",
+                ThumbnailUrl = "image/blogs/blazor/wasm/blazor-wasm-error-logging-400w.svg",
+                ContentUrl = "blogs/blazor-wasm-error-logging",
+                IconUrl = "image/icons/blazor.png",
+                Type = "Blazor",
+                CreatedOn = new DateTime(2022, 1, 23, 22, 30, 0)
+            },
+            new ContentMetaData
+            {
+                Title = "Blazor WASM Dark Mode",
+                Author = "Abdul Rahman",
+                PosterUrl = "image/blogs/blazor/wasm/blazor-wasm-dark-mode-1200w.svg 1200w",
+                ThumbnailUrl = "image/blogs/blazor/wasm/blazor-wasm-dark-mode-400w.svg",
+                ContentUrl = "blogs/blazor-wasm-dark-mode",
+                IconUrl = "image/icons/blazor.png",
+                Type = "Blazor",
+                CreatedOn = new DateTime(2022, 1, 30, 22, 30, 0)
+            },
+        };
+}
