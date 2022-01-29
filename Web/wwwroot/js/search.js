ff0a0cdb761c1f7bc6d8ed0f75b60b84d5d8d169
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-30 00:52:40 +0530

    Add Search Component

---
+++ Web/wwwroot/js/search.js
@@ -0,0 +1,18 @@
+﻿export function getViewPortWidth(dotNetHelper, dispose) {
+    if (!dispose) {
+        window.onresize = () => {
+            dotNetHelper.invokeMethodAsync('WindowResized', Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) < 640);
+        }
+    }
+    else {
+        dotNetHelper.dispose();
+    }
+
+    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+}
+
+export function focusElement(id)
+{
+    var element = document.getElementById(id);
+    element.focus();
+}
