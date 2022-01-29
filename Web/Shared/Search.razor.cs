dcc9800533771853bbd856cb5c67deaa15df2a0e
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-30 01:46:38 +0530

    Complete Search

--- Web/Shared/Search.razor.cs
+++ Web/Shared/Search.razor.cs
@@ -13,18 +13,34 @@ public class SearchBase : ComponentBase, IAsyncDisposable
     private DotNetObjectReference<SearchBase>? objRef;
     private ushort viewPortWidth;
     private short _selectedListItemIndex = -1;
+    private string _searchText = string.Empty;
 
     protected bool SmallDevice;
     protected bool HideNonSearchItems;
     protected Guid _componentId = Guid.NewGuid();
-    protected string SearchText = string.Empty;
-    protected bool ShowSuggestions => !string.IsNullOrWhiteSpace(SearchText) && FilteredContents.Count > 0;
-    protected List<ContentMetaData> FilteredContents => TableOfContents.Contents
-        .Where(content => content.Title.Contains(SearchText, StringComparison.OrdinalIgnoreCase)).ToList();
+    protected ElementReference SearchInput;
+    protected string SearchText
+    {
+        get { return _searchText; }
+        set
+        {
+            _searchText = value;
 
-    [Parameter, EditorRequired] public EventCallback<bool> ToggleNonSearchItems { get; set; }
+            if (value.Length == 0)
+            {
+                _selectedListItemIndex = -1;
+            }
+
+            ShowSuggestions = _searchText.Length > 0 && FilteredContents.Count > 0;
+        }
+    }
+    protected bool ShowSuggestions { get; set; }
+
     [Inject] private TableOfContents TableOfContents { get; set; } = default!;
     [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
+
+    [Parameter, EditorRequired] public EventCallback<bool> ToggleNonSearchItems { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -36,6 +52,9 @@ public class SearchBase : ComponentBase, IAsyncDisposable
 
         SmallDevice = viewPortWidth < SMALLDEVICEWIDTH;
     }
+
+    protected List<ContentMetaData> FilteredContents => TableOfContents.Contents
+        .Where(content => content.Title.Contains(SearchText, StringComparison.OrdinalIgnoreCase)).ToList();
 
     protected async Task Search()
     {
@@ -56,11 +75,6 @@ public class SearchBase : ComponentBase, IAsyncDisposable
         await ToggleNonSearchItem();
     }
 
-    private async Task ToggleNonSearchItem()
-    {
-        await ToggleNonSearchItems.InvokeAsync(HideNonSearchItems);
-    }
-
     protected async Task KeyUpHandler(KeyboardEventArgs eventArgs)
     {
         await KeyDownHandler(eventArgs);
@@ -70,19 +84,52 @@ public class SearchBase : ComponentBase, IAsyncDisposable
     {
         switch (eventArgs.Key)
         {
-            //case "Enter":
-            //    await OnEnterKey();
-            //    break;
+            case "Enter":
+                OnEnterKey();
+                break;
             case "ArrowDown":
                 await SelectNextItem(+1);
                 break;
             case "ArrowUp":
                 await SelectNextItem(-1);
                 break;
+            case "Escape":
+                await SearchInput.FocusAsync();
+                break;
             default:
-
                 break;
         }
+    }
+
+    protected async Task FocusOutHandler()
+    {
+        // to avoid race between mouse click of item in suggestion with OnSelect. without this delay OnSelect
+        // is not getting executed when item is clicked from mouse
+        await Task.Delay(250);
+        ShowSuggestions = _selectedListItemIndex != -1;
+        if (!ShowSuggestions)
+        {
+            _selectedListItemIndex = -1;
+        }
+    }
+
+    protected void OnSelect(ContentMetaData content)
+    {
+        SearchText = content.Title.ToLower();
+        _selectedListItemIndex = -1;
+        ShowSuggestions = false;
+        NavigationManager.NavigateTo(content.ContentUrl);
+    }
+
+    private void OnEnterKey()
+    {
+        if (ShowSuggestions == false)
+            return;
+        if (FilteredContents.Count == 0)
+            return;
+        if (_selectedListItemIndex >= 0 && _selectedListItemIndex < FilteredContents.Count)
+            OnSelect(FilteredContents[_selectedListItemIndex]);
+        return;
     }
 
     private async Task SelectNextItem(short increment)
@@ -92,6 +139,11 @@ public class SearchBase : ComponentBase, IAsyncDisposable
         _selectedListItemIndex = (short)Math.Max(0, Math.Min(FilteredContents.Count - 1, _selectedListItemIndex + increment));
         var elementIdToFocus = $"{_componentId}_{_selectedListItemIndex}";
         await module!.InvokeVoidAsync("focusElement", elementIdToFocus);
+    }
+
+    private async Task ToggleNonSearchItem()
+    {
+        await ToggleNonSearchItems.InvokeAsync(HideNonSearchItems);
     }
 
     async ValueTask IAsyncDisposable.DisposeAsync()
