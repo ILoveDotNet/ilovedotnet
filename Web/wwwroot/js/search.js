a361007748f613fd8f1c4668778041add56aa718
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-30 23:06:36 +0530

    Remove viewport calculation from search component and replace with cascading parameter from App.razor

--- Web/wwwroot/js/search.js
+++ Web/wwwroot/js/search.js
@@ -1,8 +1,4 @@
-﻿export function getViewPortWidth() {
-    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-}
-
-export function focusElement(id) {
+﻿export function focusElement(id) {
     var element = document.getElementById(id);
     element.focus();
 }
