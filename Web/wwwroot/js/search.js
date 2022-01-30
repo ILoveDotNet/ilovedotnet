55bca5edbcd44d75c5861d6b828611df27ad72ce
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-30 23:36:29 +0530

    Remove window.onresize and JSInvokable to fix search bar getting hidden on keyboard popup in mobile

--- Web/wwwroot/js/search.js
+++ Web/wwwroot/js/search.js
@@ -1,18 +1,8 @@
-﻿export function getViewPortWidth(dotNetHelper, dispose) {
-    if (!dispose) {
-        window.onresize = () => {
-            dotNetHelper.invokeMethodAsync('WindowResized', Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) < 640);
-        }
-    }
-    else {
-        dotNetHelper.dispose();
-    }
-
+﻿export function getViewPortWidth() {
     return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 }
 
-export function focusElement(id)
-{
+export function focusElement(id) {
     var element = document.getElementById(id);
     element.focus();
 }
