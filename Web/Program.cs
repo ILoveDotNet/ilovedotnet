a6cfb68525b3861e621b10bb9b580295bc5fff1f
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-01-30 19:27:36 +0530

    Remove Blazored.LocalStorage

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,5 @@
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Text.Json.Serialization;
 using Web;
 using Web.Models;
 
@@ -14,11 +12,5 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<AppState>();
 
 builder.Services.AddScoped<TableOfContents>();
-
-builder.Services.AddBlazoredLocalStorage(configuration => 
-{
-    configuration.JsonSerializerOptions.WriteIndented = true;
-    configuration.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
 
 await builder.Build().RunAsync();
