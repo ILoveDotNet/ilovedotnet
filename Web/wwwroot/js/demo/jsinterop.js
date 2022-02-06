6951da92416c2a1edc5751bf0046b92282c2e3d0
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-06 22:28:56 +0530

    Add Blazor Wasm Javascript Interop and Isolation

---
+++ Web/wwwroot/js/demo/jsinterop.js
@@ -0,0 +1,17 @@
+﻿var blazorInterop = blazorInterop || {};
+
+blazorInterop.displayAlert = (textToDisplay) => { alert(textToDisplay); };
+
+blazorInterop.getViewportDimensions = () => {
+    return { width: window.innerWidth, height: window.innerHeight };
+}
+
+blazorInterop.callDotNetStaticFromJs = () => {
+    let message = DotNet.invokeMethod('Web', 'GetMessageFromDotNet');
+    alert(`Message from .Net Static Method - ${message}`);
+}
+
+blazorInterop.callDotNetInstanceFromJs = (dotNetHelper) => {
+    let message = dotNetHelper.invokeMethod('GetMessageFromDotNet');
+    alert(`Message from .Net Static Method - ${message}`);
+}
