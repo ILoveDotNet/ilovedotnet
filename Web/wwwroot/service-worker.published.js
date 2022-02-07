883748d42689c0bf14a1aa04d1d7226ef48111a0
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-07 14:29:21 +0530

    Fix broken service worker issue

--- Web/wwwroot/service-worker.published.js
+++ Web/wwwroot/service-worker.published.js
@@ -4,9 +4,9 @@
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-//self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
-const cacheVersion = 20220130223000;
+const cacheVersion = 20220206223000;
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}${cacheVersion}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
