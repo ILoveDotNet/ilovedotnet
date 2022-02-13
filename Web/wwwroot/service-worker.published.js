387ac962cf0a85b4c2409ad4b157db15e597cfdb
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-13 20:23:22 +0530

    Update service-worker cache key

--- Web/wwwroot/service-worker.published.js
+++ Web/wwwroot/service-worker.published.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
-const cacheVersion = 20220206223000;
+const cacheVersion = 20220213223000;
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}${cacheVersion}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
