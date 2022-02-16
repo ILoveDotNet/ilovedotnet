46a70ed8a15f3929479ed21b4c48269f5c71f7d1
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-16 23:27:19 +0530

    Remove Components

--- Web/Components/DemoSnippet.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace Web.Components;
-
-public class DemoSnippetBase : ComponentBase
-{
-    [Parameter, EditorRequired] public string Title { get; set; } = default!;
-    [Parameter, EditorRequired] public RenderFragment DemoContent { get; set; } = default!;
-}
