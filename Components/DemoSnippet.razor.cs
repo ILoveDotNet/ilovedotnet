5c5dd4c5906d89aa33cb845a76fdd9917043d19e
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-16 23:28:55 +0530

    Add Components to Lazy Load Assembly

---
+++ Components/DemoSnippet.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Components;
+
+public class DemoSnippetBase : ComponentBase
+{
+    [Parameter, EditorRequired] public string Title { get; set; } = default!;
+    [Parameter, EditorRequired] public RenderFragment DemoContent { get; set; } = default!;
+}
