07e8955546d730038c0eee217f9e993bd60733cd
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-26 23:57:54 +0530

    Remove service worker cache version

--- Web/wwwroot/service-worker.published.js
+++ Web/wwwroot/service-worker.published.js
@@ -6,9 +6,8 @@ self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
-const cacheVersion = 20220220223000;
 const cacheNamePrefix = 'offline-cache-';
-const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}${cacheVersion}`;
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
