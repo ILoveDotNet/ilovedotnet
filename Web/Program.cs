75ff5549dd335cdb2938c56b4de9fbb20c2ce659
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-02-27 20:24:03 +0530

    Add Prerendering Support

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 using Web;
 using Web.Models;
@@ -8,12 +9,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddScoped<AppState>();
-
-builder.Services.AddScoped<TableOfContents>();
-
-builder.Services.AddHeadElementHelper();
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+    services.AddScoped<AppState>();
+
+    services.AddScoped<TableOfContents>();
+
+    services.AddScoped<LazyAssemblyLoader>();
+
+    services.AddHeadElementHelper();
+}
