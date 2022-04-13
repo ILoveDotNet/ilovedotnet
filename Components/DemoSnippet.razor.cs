7b10e8681e11c4cb30d4623eff01d0d1b33f69cb
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-04-13 07:20:35 +0530

    Rename DemoContent RenderFragment Parameter to ChildContent

--- Components/DemoSnippet.razor.cs
+++ Components/DemoSnippet.razor.cs
@@ -5,5 +5,5 @@ namespace Components;
 public class DemoSnippetBase : ComponentBase
 {
     [Parameter, EditorRequired] public string Title { get; set; } = default!;
-    [Parameter, EditorRequired] public RenderFragment DemoContent { get; set; } = default!;
+    [Parameter, EditorRequired] public RenderFragment ChildContent { get; set; } = default!;
 }
