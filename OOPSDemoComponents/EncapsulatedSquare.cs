d7f78df403d42f1131c431a838b074cf9afaf7c0
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-05-01 09:12:03 +0530

    Add Edge, Square and Encapsulated Square

---
+++ OOPSDemoComponents/EncapsulatedSquare.cs
@@ -0,0 +1,14 @@
+﻿namespace OOPSDemoComponents;
+
+public class EncapsulatedSquare
+{
+    public EncapsulatedSquare(IEnumerable<Edge> edges)
+    {
+        if (edges.Count() != 4)
+            throw new Exception("Square must have 4 edges");
+
+        Edges = edges;
+    }
+
+    public IEnumerable<Edge> Edges { get; }
+}
