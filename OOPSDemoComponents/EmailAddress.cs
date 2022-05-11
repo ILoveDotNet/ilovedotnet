2715665595614de8925c27f176245aca510d1fc0
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-05-11 21:32:51 +0530

    Add OOPS Abstraction Demo Components

---
+++ OOPSDemoComponents/EmailAddress.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OOPSDemoComponents;
+
+public class EmailAddress
+{
+    public string Value { get; }
+
+    private EmailAddress(string value)
+    {
+        Value = value;
+    }
+
+    public static EmailAddress Create(string address)
+    {
+        if (!Regex.IsMatch(address, @"^(.+)@(.+$)"))
+            throw new Exception();
+
+        return new EmailAddress(address);
+    }
+}
