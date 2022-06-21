4b3acafda763cb1af0bbabf74c9522ff613ecd8c
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-06-21 07:12:32 +0530

    Add Navigation Menu Toggle

--- Web/Models/AppState.cs
+++ Web/Models/AppState.cs
@@ -2,6 +2,14 @@
 
 internal class AppState
 {
+    public bool ShowNavigation { get; private set; } = true;
+
+    public void ToggleSideBar() 
+    {
+        ShowNavigation = !ShowNavigation;
+        NotifyStateChanged();
+    }
+    
     internal event Action OnChange = default!;
 
     private void NotifyStateChanged() => OnChange?.Invoke();
