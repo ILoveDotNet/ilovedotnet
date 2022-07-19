38b68478674ac81732ddd98c095d8a5ec8b52eae
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-07-19 16:36:49 +0530

    Complete Dependency Injection Lifetime Demo

--- SharedModels/TableOfContents.cs
+++ SharedModels/TableOfContents.cs
@@ -372,6 +372,19 @@ public class TableOfContents
                 Type = "Dependency-Injection",
                 CreatedOn = new DateTime(2022, 7, 24, 22, 30, 0),
                 ModifiedOn = new DateTime(2022, 7, 24, 22, 30, 0)
+            },
+            new ContentMetaData
+            {
+                Order = 2,
+                Title = "Dependency Injection Lifetimes in .NET",
+                Author = "Abdul Rahman",
+                PosterUrl = "image/blogs/dependency-injection/dependency-injection-lifetimes-in-dotnet.svg",
+                ThumbnailUrl = "image/blogs/dependency-injection/dependency-injection-lifetimes-in-dotnet.svg",
+                ContentUrl = "blogs/dependency-injection-lifetimes-in-dotnet",
+                IconUrl = "image/icons/dependency-injection.png",
+                Type = "Dependency-Injection",
+                CreatedOn = new DateTime(2022, 7, 31, 22, 30, 0),
+                ModifiedOn = new DateTime(2022, 7, 31, 22, 30, 0)
             }
         };
     }
