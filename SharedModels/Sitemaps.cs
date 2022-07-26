38a2aecce5c5707ca20b895a8d3733386a7c5797
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-07-26 13:23:42 +0530

    Add all sitemaps in ContentHead and in other static pages

---
+++ SharedModels/Sitemaps.cs
@@ -0,0 +1,16 @@
+﻿namespace SharedModels;
+
+public class Sitemaps
+{
+    public IReadOnlyList<string> Files => new List<string>(9) {
+        "sitemap.xml",
+        "sitemap-blog-blazor-wasm.xml",
+        "sitemap-blog-dependency-injection.xml",
+        "sitemap-blog-middleware.xml",
+        "sitemap-blog-oops.xml",
+        "sitemap-blog-tdd.xml",
+        "sitemap-blog-webapi.xml",
+        "sitemap-channels.xml",
+        "sitemap-talk.xml"
+    };
+}
