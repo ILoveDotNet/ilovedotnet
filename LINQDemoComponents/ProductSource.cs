b7394710dd44abf0f9646c67745ecc77896d8dc2
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-08-14 08:35:42 +0530

    Add LINQDemoComponents Project

---
+++ LINQDemoComponents/ProductSource.cs
@@ -0,0 +1,25 @@
+﻿namespace LINQDemoComponents;
+
+internal class ProductSource
+{
+    private readonly List<Product> FullProducts = new(31);
+    public IReadOnlyList<Product> Products => FullProducts.ToList();
+
+    public ProductSource()
+    {
+        FullProducts =
+            new()
+            {
+                new Product
+                {
+                    Name = "IPhone",
+                    Price = 100000
+                },
+                new Product
+                {
+                    Name = "Google",
+                    Price = 70000
+                }
+            };
+    }
+}
