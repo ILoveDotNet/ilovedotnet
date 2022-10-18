9c61be4cc1f5113442fdcb061cc4dd8da1e0ac68
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-10-19 00:04:45 +0530

    Add products in products source

--- LINQDemoComponents/ProductSource.cs
+++ LINQDemoComponents/ProductSource.cs
@@ -25,6 +25,38 @@ public class ProductSource
                     Color = "Red",
                     Price = 1000m,
                     Size = "58"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "HL Road Frame",
+                    Color = "Black",
+                    Price = 1000m,
+                    Size = "58"
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "HL Road Frame",
+                    Color = "Red",
+                    Price = 1000m,
+                    Size = "58"
+                },
+                new Product
+                {
+                    Id = 5,
+                    Name = "HL Road Frame",
+                    Color = "Brown",
+                    Price = 1000m,
+                    Size = "58"
+                },
+                new Product
+                {
+                    Id = 6,
+                    Name = "HL Road Frame",
+                    Color = "White",
+                    Price = 1000m,
+                    Size = "58"
                 }
             };
     }
