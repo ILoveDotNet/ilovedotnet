0e34b759f121bdcd379773475f4c672e918f3176
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-10-23 07:57:32 +0530

    Move GoogleAdSense to SharedComponents

--- SharedComponents/GoogleAdSense.razor.cs
+++ SharedComponents/GoogleAdSense.razor.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Components;
+namespace SharedComponents;
 
 public class GoogleAdSenseBase : ComponentBase
 {
