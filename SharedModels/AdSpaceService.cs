a6c2a49340592feba9a889e32df6bdee9d4691ae
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-10-24 18:49:50 +0530

    Add AdSpaceService

---
+++ SharedModels/AdSpaceService.cs
@@ -0,0 +1,16 @@
+﻿namespace SharedModels;
+
+public class AdSpaceService 
+{
+    private readonly Random random = new();
+
+    public readonly List<int> AdSpace = new();
+
+    public AdSpaceService(TableOfContents tableOfContents)
+    {
+        for (int i = 4; i <= tableOfContents.Contents.Count - 4; i += 4)
+        {
+            AdSpace.Add(random.Next(i, i + 5));
+        }
+    }
+}
