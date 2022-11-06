84f873d5deab9d199a90e1de63f9b47ca9311b80
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-11-06 19:15:57 +0530

    LINQ Contains Completed

---
+++ LINQDemoComponents/ProductIdComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LINQDemoComponents;
+
+public class ProductIdComparer : EqualityComparer<Product>
+{
+    public override bool Equals(Product? product, Product? anotherProduct)
+    {
+        return product?.Id == anotherProduct?.Id;
+    }
+
+    public override int GetHashCode([DisallowNull] Product product)
+    {
+        return product.Id.GetHashCode();
+    }
+}
