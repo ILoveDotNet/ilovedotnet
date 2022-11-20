ac8c48c08f07c9cca9b3a3e5988b8d639d838c02
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-11-20 17:38:22 +0530

    Fix failing test

--- UITests/Components/Navigation/NavigationTests.cs
+++ UITests/Components/Navigation/NavigationTests.cs
@@ -38,24 +38,14 @@ public class NavigationTests
                                     </svg>
                                     <span class="[ text-xs text-center ]">Learning Path</span>
                                 </a>
-                                <a class="[ flex flex-col items-center ]" href="/disclaimer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="[ icon icon-tabler icon-tabler-notes ]" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <rect x="5" y="3" width="14" height="18" rx="2"></rect>
-                                        <line x1="9" y1="7" x2="15" y2="7"></line>
-                                        <line x1="9" y1="11" x2="15" y2="11"></line>
-                                        <line x1="9" y1="15" x2="13" y2="15"></line>
-                                    </svg>
-                                    <span class="[ text-xs text-center ]">Disclaimer</span>
-                                </a>
-                                <a class="[ flex flex-col items-center ]" href="/privacy">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="[ icon icon-tabler icon-tabler-lock ]" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <rect x="5" y="11" width="14" height="10" rx="2"></rect>
-                                        <circle cx="12" cy="16" r="1"></circle>
-                                        <path d="M8 11v-4a4 4 0 0 1 8 0v4"></path>
-                                    </svg>
-                                    <span class="[ text-xs text-center ]">Privacy</span>
+                                <a class="[ flex flex-col items-center ]" href="/about">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="[ icon icon-tabler icon-tabler-info-circle ]" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                       <circle cx="12" cy="12" r="9"></circle>
+                                       <line x1="12" y1="8" x2="12.01" y2="8"></line>
+                                       <polyline points="11 12 12 12 12 16 13 16"></polyline>
+                                   </svg>
+                                    <span class="[ text-xs text-center ]">About</span>
                                 </a>
                             </nav>
                            """);
