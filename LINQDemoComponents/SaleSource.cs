1377b0f8b1b7ee561dfd743183e66b65983f3445
Abdul Rahman <abdulrahman.smsi@gmail.com>
2022-12-05 00:35:04 +0530

    Complete LINQ Except and ExceptBy

---
+++ LINQDemoComponents/SaleSource.cs
@@ -0,0 +1,25 @@
+﻿namespace LINQDemoComponents;
+
+public class SaleSource
+{
+    private readonly List<Sale> FullSales = new(2);
+    public IReadOnlyList<Sale> Sales => FullSales.ToList();
+
+    public SaleSource()
+    {
+        FullSales =
+            new()
+            {
+                new Sale
+                {
+                    ProductId = 1,
+                    Price = 1000m
+                },
+                new Sale
+                {
+                    ProductId = 2,
+                    Price = 1000m
+                }
+            };
+    }
+}
