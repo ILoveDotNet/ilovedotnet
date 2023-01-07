919178470c6f57ef71a45fa123d2be35453a84bd
Abdul Rahman <abdulrahman.smsi@gmail.com>
2023-01-07 22:31:11 +0530

    Complete LINQ Join

--- LINQDemoComponents/Sale.cs
+++ LINQDemoComponents/Sale.cs
@@ -2,6 +2,8 @@
 
 public class Sale
 {
+    public int Id { get; set; }
     public int ProductId { get; set; }
     public decimal Price { get; set; }
+    public string ProductColor { get; set; } = default!;
 }
