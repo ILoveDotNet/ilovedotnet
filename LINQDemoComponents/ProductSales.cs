24d1315b7b51bd2227cad2e702fff9d1a8475c6f
Abdul Rahman <abdulrahman.smsi@gmail.com>
2023-01-15 22:17:53 +0530

    LINQ Group Join Corrections

--- LINQDemoComponents/ProductSales.cs
+++ LINQDemoComponents/ProductSales.cs
@@ -3,5 +3,5 @@ namespace LINQDemoComponents;
 public class ProductSales 
 {
     public Product Product { get; set; } = default!;
-    public IEnumerable<Sale> Sales { get; set; } = default!;
+    public List<Sale> Sales { get; set; } = default!;
 }
