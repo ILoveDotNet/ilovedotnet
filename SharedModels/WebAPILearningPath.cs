d90dc6a3f2d1e2dcf0b45b836937c327ea24d0e2
Abdul Rahman <abdulrahman.smsi@gmail.com>
2023-06-04 16:22:29 +0530

    Completed Unit Testing Middlewares in ASP.NET WebAPI

--- SharedModels/WebAPILearningPath.cs
+++ SharedModels/WebAPILearningPath.cs
@@ -2,12 +2,12 @@
 
 public class WebAPILearningPath
 {
-    public readonly List<ContentMetaData> FullContents = new(3);
+    public readonly List<ContentMetaData> FullContents = new(4);
 
     public WebAPILearningPath()
     {
         FullContents =
-        new(3)
+        new(4)
         {
             new ContentMetaData
             {
@@ -47,6 +47,19 @@ public class WebAPILearningPath
                 Type = "WebAPI",
                 CreatedOn = new DateTime(2023, 5, 28, 22, 30, 0),
                 ModifiedOn = new DateTime(2023, 5, 28, 22, 30, 0)
+            },
+            new ContentMetaData
+            {
+                Order = 4,
+                Title = "Unit Testing Middlewares in ASP.NET Web API",
+                Author = "Abdul Rahman",
+                PosterUrl = "image/blogs/webapi/unit-testing-middlewares-in-asp-net-webapi.png",
+                ThumbnailUrl = "image/blogs/webapi/unit-testing-middlewares-in-asp-net-webapi.png",
+                ContentUrl = "blogs/unit-testing-middlewares-in-asp-net-webapi",
+                IconUrl = "image/icons/webapi.png",
+                Type = "WebAPI",
+                CreatedOn = new DateTime(2023, 6, 4, 22, 30, 0),
+                ModifiedOn = new DateTime(2023, 6, 4, 22, 30, 0)
             }
         };
     }
