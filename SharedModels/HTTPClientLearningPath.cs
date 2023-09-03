a1e4a154008b071ccf4847b996f31da35eaf33d0
Abdul Rahman <abdulrahman.smsi@gmail.com>
2023-09-04 00:32:23 +0530

    Completed Save Bandwidth with Compression Support in HttpClient

--- SharedModels/HTTPClientLearningPath.cs
+++ SharedModels/HTTPClientLearningPath.cs
@@ -2,12 +2,12 @@ namespace SharedModels;
 
 public class HTTPClientLearningPath
 {
-    public readonly List<ContentMetaData> FullContents = new(2);
+    public readonly List<ContentMetaData> FullContents = new(3);
 
     public HTTPClientLearningPath()
     {
         FullContents =
-        new(2)
+        new(3)
         {
             new ContentMetaData
             {
@@ -34,6 +34,19 @@ public class HTTPClientLearningPath
                 Type = "HTTP-Client",
                 CreatedOn = new DateTime(2023, 8, 27, 22, 30, 0),
                 ModifiedOn = new DateTime(2023, 8, 27, 22, 30, 0)
+            },
+            new ContentMetaData
+            {
+                Order = 3,
+                Title = "Save bandwidth with Compression when sending and reading data using HTTPClient in dotnet",
+                Author = "Abdul Rahman",
+                PosterUrl = "image/blogs/http-client/save-bandwidth-with-compression-when-sending-and-reading-data-using-http-client-in-dotnet.png",
+                ThumbnailUrl = "image/blogs/http-client/save-bandwidth-with-compression-when-sending-and-reading-data-using-http-client-in-dotnet.png",
+                ContentUrl = "blogs/save-bandwidth-with-compression-when-sending-and-reading-data-using-http-client-in-dotnet",
+                IconUrl = "image/icons/http-client.png",
+                Type = "HTTP-Client",
+                CreatedOn = new DateTime(2023, 9, 3, 22, 30, 0),
+                ModifiedOn = new DateTime(2023, 9, 3, 22, 30, 0)
             },
         };
     }
--- SharedModels/TableOfContents.cs
+++ SharedModels/TableOfContents.cs
@@ -3,7 +3,7 @@
 public class TableOfContents
 {
     public const int PageSize = 6;
-    private readonly List<ContentMetaData> FullContents = new(85);
+    private readonly List<ContentMetaData> FullContents = new(86);
     public IReadOnlyList<ContentMetaData> Contents 
             => FullContents
                 .Where(content => content.CreatedOn.Date <= DateTime.Today.Date)
