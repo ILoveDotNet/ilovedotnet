fced755410da4618b5c5f6a50595cb321df02c4c
Abdul Rahman <abdulrahman.smsi@gmail.com>
2024-02-11 20:13:12 +0530

    Completed updating Excel report generation using closed xml

--- SharedModels/ReportLearningPath.cs
+++ SharedModels/ReportLearningPath.cs
@@ -19,8 +19,8 @@ public class ReportLearningPath
                 ContentUrl = "blogs/generate-excel-report-using-closed-xml-in-dotnet",
                 IconUrl = "image/icons/report.webp",
                 Type = "Report",
-                CreatedOn = new DateTime(2023, 1, 29, 22, 30, 0),
-                ModifiedOn = new DateTime(2023, 1, 29, 22, 30, 0)
+                CreatedOn = new DateTime(2024, 2, 11, 22, 30, 0),
+                ModifiedOn = new DateTime(2024, 2, 11, 22, 30, 0)
             },
             new ContentMetaData
             {
