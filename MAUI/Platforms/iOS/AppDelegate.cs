32323329ca29b3863eafb0bbd1bbf28516ebb050
Abdul Rahman <abdulrahman.smsi@gmail.com>
2024-03-10 20:14:29 +0530

    Revert "Add MAUI Blazor Hybrid Project and Update deployment.yml"
    This reverts commit acecf1f856a56b7d368adfa4a9f9f1eeaa3679f5.

--- MAUI/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace MAUI;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
