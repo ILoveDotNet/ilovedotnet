c64c4e9f82ed7c2cd681461f1b4f9ac30e69a1a7
Abdul Rahman <abdulrahman.smsi@gmail.com>
2024-03-24 20:01:40 +0530

    Use default navigation manager instead of scroll js

--- SharedComponents/FragmentNavigationBase.cs
+++ SharedComponents/FragmentNavigationBase.cs
@@ -1,43 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.JSInterop;
 
 namespace SharedComponents;
 
-public class FragmentNavigationBase : ComponentBase, IAsyncDisposable
+public class FragmentNavigationBase : ComponentBase
 {
-    private IJSObjectReference? module;
-
     [Inject] private NavigationManager NavigationManager { get; set; } = default!;
-    [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
 
-    protected override void OnInitialized()
-    {
-        NavigationManager.LocationChanged += TryFragmentNavigation!;
-    }
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
+    protected override void OnAfterRender(bool firstRender)
     {
         if (firstRender)
         {
-            module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/scrollto.js");
-
-            await NavigationManager.NavigateToFragmentAsync(module);
+            NavigationManager.NavigateTo(NavigationManager.Uri);
         }
-    }
-
-    private async void TryFragmentNavigation(object sender, LocationChangedEventArgs args)
-    {
-        await NavigationManager.NavigateToFragmentAsync(module);
-    }
-
-    async ValueTask IAsyncDisposable.DisposeAsync()
-    {
-        if (module is not null)
-        {
-            await module.DisposeAsync();
-        }
-
-        NavigationManager.LocationChanged -= TryFragmentNavigation!;
     }
 }
