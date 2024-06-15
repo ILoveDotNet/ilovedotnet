8f6b653006a00294210edd4980022ecd93177aa3
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2024-06-16 00:14:55 +0530

    Completed Nuget Vulnerability Check

--- SharedModels/OWASPLearningPath.cs
+++ SharedModels/OWASPLearningPath.cs
@@ -12,7 +12,7 @@ public class OWASPLearningPath
             {
                 Order = 1,
                 Title = "OWASP - Secure your dotnet app by scanning for vulnerable nuget dependencies in CI pipelines",
-                Description = "In this post I will teach you one of the owasp top 10 requirement to verify code to avoid malicious dependencies. All with live working demo.",
+                Description = "In this post I will teach you one of the owasp top 10 requirement to verify code to avoid malicious dependencies using nuget audit. All with live working demo.",
                 Author = "Abdul Rahman",
                 Slug = "owasp-secure-your-dotnet-app-by-scanning-for-vulnerable-nuget-dependency-in-ci-pipelines",
                 PosterUrl = "image/blogs/owasp/owasp-secure-your-dotnet-app-by-scanning-for-vulnerable-nuget-dependency-in-ci-pipelines.webp",
@@ -21,8 +21,8 @@ public class OWASPLearningPath
                 IconUrl = "image/icons/owasp.webp",
                 Type = "OWASP",
                 CreatedOn = new DateTime(2024, 2, 25, 22, 30, 0, DateTimeKind.Utc),
-                ModifiedOn = new DateTime(2024, 2, 25, 22, 30, 0, DateTimeKind.Utc),
-                Keywords = ["Security", "Dependency Check", "Nuget Vulnerability", "Malicious Code"]
+                ModifiedOn = new DateTime(2024, 6, 16, 22, 30, 0, DateTimeKind.Utc),
+                Keywords = ["Security", "Dependency Check", "Nuget Vulnerability", "Malicious Code", "Audit Nuget"]
             }
         ];
     }
