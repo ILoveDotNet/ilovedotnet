4baacb78a551588f92b008df07fcf7dacf5eba18
Abdul Rahman <abdulrahman.smsi@gmail.com>
2024-07-07 17:16:54 +0530

    Added July Search Impressions

--- SharedModels/Achievements.cs
+++ SharedModels/Achievements.cs
@@ -51,6 +51,7 @@ public class Achievements
             new() { Date = new DateOnly(2024, 3, 31), Impressions = 102000 },
             new() { Date = new DateOnly(2024, 4, 30), Impressions = 91300 },
             new() { Date = new DateOnly(2024, 5, 31), Impressions = 86300 },
+            new() { Date = new DateOnly(2024, 6, 30), Impressions = 82400 },
         ];
     }    
 }
