29e3d6e6175dc52033c1ad22fa3c706d100cff20
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2024-09-22 19:27:49 +0530

    updated introduction to tdd in .net

--- SharedModels/TDDLearningPath.cs
+++ SharedModels/TDDLearningPath.cs
@@ -1,4 +1,4 @@
-﻿namespace SharedModels;
+namespace SharedModels;
 
 public class TDDLearningPath
 {
@@ -21,8 +21,8 @@ public class TDDLearningPath
                 IconUrl = "image/icons/tdd.webp",
                 Channel = "TDD",
                 Type = "blogs",
-                CreatedOn = new DateTime(2022, 3, 27, 22, 30, 0, DateTimeKind.Utc),
-                ModifiedOn = new DateTime(2022, 3, 27, 22, 30, 0, DateTimeKind.Utc),
+                CreatedOn = new DateTime(2024, 9, 22, 22, 30, 0, DateTimeKind.Utc),
+                ModifiedOn = new DateTime(2024, 9, 22, 22, 30, 0, DateTimeKind.Utc),
                 Keywords = ["Test Driven Development"]
             },
             new ContentMetaData
