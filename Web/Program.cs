fe3bc2c47d0cea9355631bddc6450b547386b5bd
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2024-12-01 22:19:29 +0530

    disable oidc as it gives prerender publish error in ci

--- Web/Program.cs
+++ Web/Program.cs
@@ -89,24 +89,24 @@ static void ConfigureServices(IServiceCollection services, string baseAddress, I
 
     services.AddScoped(serviceProvider => new SlugService(serviceProvider.GetRequiredService<NavigationManager>()));
 
-    services.AddOidcAuthentication(options =>
-    {
-        options.ProviderOptions.Authority = "https://demo.duendesoftware.com";
-        options.ProviderOptions.ClientId = "interactive.public.short";
-        options.ProviderOptions.ResponseType = "code";
-        options.ProviderOptions.DefaultScopes.Add("openid");
-        options.ProviderOptions.DefaultScopes.Add("profile");
-        options.ProviderOptions.DefaultScopes.Add("email");
-        options.ProviderOptions.DefaultScopes.Add("offline_access");
-        options.ProviderOptions.DefaultScopes.Add("api");
-        options.ProviderOptions.RedirectUri = $"{baseAddress}authentication/login-callback";
-        options.ProviderOptions.PostLogoutRedirectUri = $"{baseAddress}authentication/logout-callback";
+    // services.AddOidcAuthentication(options =>
+    // {
+    //     options.ProviderOptions.Authority = "https://demo.duendesoftware.com";
+    //     options.ProviderOptions.ClientId = "interactive.public.short";
+    //     options.ProviderOptions.ResponseType = "code";
+    //     options.ProviderOptions.DefaultScopes.Add("openid");
+    //     options.ProviderOptions.DefaultScopes.Add("profile");
+    //     options.ProviderOptions.DefaultScopes.Add("email");
+    //     options.ProviderOptions.DefaultScopes.Add("offline_access");
+    //     options.ProviderOptions.DefaultScopes.Add("api");
+    //     options.ProviderOptions.RedirectUri = $"{baseAddress}authentication/login-callback";
+    //     options.ProviderOptions.PostLogoutRedirectUri = $"{baseAddress}authentication/logout-callback";
 
-        options.UserOptions.RoleClaim = "role";
-        options.UserOptions.NameClaim = "name";
-    });
+    //     options.UserOptions.RoleClaim = "role";
+    //     options.UserOptions.NameClaim = "name";
+    // });
 
-    services.AddAuthorizationCore();
+    // services.AddAuthorizationCore();
 
-    services.AddCascadingAuthenticationState();
+    // services.AddCascadingAuthenticationState();
 }
