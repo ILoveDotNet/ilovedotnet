7c296858c051d751a41d83a9feb03cfa04b21da6
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2024-12-08 06:54:10 +0530

    disable demo in prerendering

--- BaseComponents/DemoSnippet.razor.cs
+++ BaseComponents/DemoSnippet.razor.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using SharedModels;
 
 namespace BaseComponents;
 
 public class DemoSnippetBase : ComponentBase
 {
+    [Inject] public IHostEnvironment Environment { get; set; } = default!;
+
     [Parameter, EditorRequired] public string Title { get; set; } = default!;
     [Parameter, EditorRequired] public RenderFragment ChildContent { get; set; } = default!;
 }
