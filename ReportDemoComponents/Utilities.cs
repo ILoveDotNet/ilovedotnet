02e509269e3714f72dbbf9fed25344a968cc4e49
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2024-12-14 12:26:50 +0530

    add simple excel download demo using closed xml

---
+++ ReportDemoComponents/Utilities.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+using System.Data;
+
+namespace ReportDemoComponents;
+
+public static class Utilities
+{
+    public static DataTable ToDataTable<T>(this IEnumerable<T> data, string name)
+    {
+        PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T)!);
+        DataTable table = new(name);
+
+        foreach (PropertyDescriptor prop in properties)
+            table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+
+        foreach (T item in data)
+        {
+            DataRow row = table.NewRow();
+
+            foreach (PropertyDescriptor prop in properties)
+                row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+
+            table.Rows.Add(row);
+        }
+
+        return table;
+    }
+}
