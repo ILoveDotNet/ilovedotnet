d01d97989ae0a6e3de5e07497c0a9acc3ded8d26
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2024-12-31 12:41:36 +0530

    update feed generation with additional metadata

--- RSSFeedGenerator/Program.cs
+++ RSSFeedGenerator/Program.cs
@@ -6,24 +6,27 @@ using SharedModels;
 var option = CommandLineSwitch.Parse<CommandLineOptions>(ref args);
 
 var feed = new SyndicationFeed(
-                "I ❤️ DotNet", 
-                "This is a .NET knowledge sharing platform with live demos crafted by developers for developers with love.", 
+                "I ❤️ DotNet",
+                "This is a .NET knowledge sharing platform with live demos crafted by developers for developers with love.",
                 new Uri("http://ilovedotnet.org"),
                 "ilovedotnet",
-                DateTimeOffset.UtcNow);
-
-var tableOfContents = new TableOfContents();
-
-var items = tableOfContents
+                DateTimeOffset.UtcNow)
+{
+    Copyright = new TextSyndicationContent($"Copyright {DateTime.UtcNow.Year}"),
+    Language = "en-IN",
+    Items = new TableOfContents()
             .AllContents
             .Select(content => new SyndicationItem(
                 content.Title, 
                 content.Description, 
                 new Uri($"http://ilovedotnet.org/{content.Type}/{content.Slug}"),
                 content.Slug,
-                content.ModifiedOn));
+                content.ModifiedOn)),
+    ImageUrl = new Uri("https://ilovedotnet.org/image/brand/mini-logo.png"),
+};
 
-feed.Items = items;
+var person = new SyndicationPerson("abdulrahman.smsi+ilovedotnet@gmail.com", "Abdul Rahman", "https://linkedin.com/in/thebhai");
+feed.Authors.Add(person);
 
 using var writer = XmlWriter.Create(option.OutputPath ?? "feed.xml", new XmlWriterSettings { Indent = true });
 var rssFormatter = new Rss20FeedFormatter(feed);
