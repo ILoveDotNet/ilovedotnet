da70f864386887b4e458e59708374b39281ea8f5
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2025-01-12 20:12:37 +0530

    added e2e test setup

---
+++ E2E/EndToEndTests/HomePageTests.cs
@@ -0,0 +1,15 @@
+﻿namespace EndToEndTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+  [Test]
+  public async Task HomepageHasCorrectContentAsync()
+  {
+    await Page.GotoAsync("https://localhost:7176/");
+
+    // Expect a title "to contain" a substring.
+    await Expect(Page).ToHaveTitleAsync(new Regex("I ❤️ DotNet"));
+  }
+}
