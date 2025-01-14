24780ff7985908d48c3277c4507618e622958695
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2025-01-14 20:05:58 +0530

    wip - add trace analysis in e2e testing in blazor wasm

--- E2E/EndToEndTests/WorkflowTests.cs
+++ E2E/EndToEndTests/WorkflowTests.cs
@@ -8,6 +8,33 @@ namespace EndToEndTests;
 [TestFixture]
 public class WorkflowTests : BaseTest
 {
+  [SetUp]
+  public async Task Setup()
+  {
+      await Context.Tracing.StartAsync(new()
+      {
+          Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+          Screenshots = true,
+          Snapshots = true,
+          Sources = true
+      });
+  }
+
+  [TearDown]
+  public async Task TearDown()
+  {
+      // This will produce e.g.:
+      // bin/Debug/net8.0/playwright-traces/LoggedInCheckOutCancellation.zip
+      await Context.Tracing.StopAsync(new()
+      {
+          Path = Path.Combine(
+              TestContext.CurrentContext.WorkDirectory,
+              "playwright-traces",
+              $"{TestContext.CurrentContext.Test.Name}.zip"
+          )
+      });
+  }
+
   [Test]
   public async Task CompleteLoginWorkFlowAsync()
   {
