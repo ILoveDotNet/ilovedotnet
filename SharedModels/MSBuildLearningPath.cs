a86687241670b2be03f863e84354bfcfbaec0910
abdulrahmanshabeekmohamed <abdulrahman.shabeekmohamed@rxglobal.com>
2025-01-28 23:01:18 +0530

    Completed - Automating Git Hook Setup using MSBuild

--- SharedModels/MSBuildLearningPath.cs
+++ SharedModels/MSBuildLearningPath.cs
@@ -8,23 +8,23 @@ public class MSBuildLearningPath
   {
     FullContents =
     [
-        new ContentMetaData
-            {
-                Order = 1,
-                Title = "Automating Git Hook Setup in .NET Projects with MSBuild",
-                Description = "In this post I will introduce how to automate setting up git hooks using MSBuild in dotnet apps. All with live working demo.",
-                Author = "Abdul Rahman",
-                Slug = "automating-git-hook-setup-in-dotnet-with-msbuild",
-                PosterUrl = "image/blogs/msbuild/automating-git-hook-setup-in-dotnet-with-msbuild.webp",
-                ThumbnailUrl = "image/blogs/msbuild/automating-git-hook-setup-in-dotnet-with-msbuild.webp",
-                ContentUrl = "blogs/automating-git-hook-setup-in-dotnet-with-msbuild",
-                IconUrl = "image/icons/msbuild.webp",
-                Channel = "MSBuild",
-                Type = "blogs",
-                CreatedOn = new DateTime(2025, 2, 2, 22, 30, 0, DateTimeKind.Utc),
-                ModifiedOn = new DateTime(2025, 2, 2, 22, 30, 0, DateTimeKind.Utc),
-                Keywords = ["Git Hook", "Pre-Commit"]
-            }
+      new ContentMetaData
+        {
+          Order = 1,
+          Title = "Automating Git Hook Setup in .NET Projects with MSBuild",
+          Description = "In this post I will introduce how to automate setting up git hooks using MSBuild in dotnet apps. All with live working demo.",
+          Author = "Abdul Rahman",
+          Slug = "automating-git-hook-setup-in-dotnet-with-msbuild",
+          PosterUrl = "image/blogs/msbuild/automating-git-hook-setup-in-dotnet-with-msbuild.webp",
+          ThumbnailUrl = "image/blogs/msbuild/automating-git-hook-setup-in-dotnet-with-msbuild.webp",
+          ContentUrl = "blogs/automating-git-hook-setup-in-dotnet-with-msbuild",
+          IconUrl = "image/icons/msbuild.webp",
+          Channel = "MSBuild",
+          Type = "blogs",
+          CreatedOn = new DateTime(2025, 2, 2, 22, 30, 0, DateTimeKind.Utc),
+          ModifiedOn = new DateTime(2025, 2, 2, 22, 30, 0, DateTimeKind.Utc),
+          Keywords = ["Git Hook", "Pre-Commit"]
+        }
     ];
   }
 }
