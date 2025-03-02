04138a0d8c9eb1280847e069b988df00decaa5e6
Abdul Rahman <abdulrahman.smsi@gmail.com>
2025-03-02 16:17:18 +0530

    Completed Setup MAUI

--- SharedModels/MAUILearningPath.cs
+++ SharedModels/MAUILearningPath.cs
@@ -15,15 +15,15 @@ public class MAUILearningPath
           Description = "In this post I will teach you how to setup .NET MAUI with detailed steps using Visual Studio Code. All with live working demo.",
           Author = "Abdul Rahman",
           Slug = "step-by-step-setup-guide-for-dotnet-maui-with-visual-studio-code",
-          PosterUrl = "image/blogs/msbuild/step-by-step-setup-guide-for-dotnet-maui-with-visual-studio-code.webp",
-          ThumbnailUrl = "image/blogs/msbuild/step-by-step-setup-guide-for-dotnet-maui-with-visual-studio-code.webp",
+          PosterUrl = "image/blogs/maui/step-by-step-setup-guide-for-dotnet-maui-with-visual-studio-code.webp",
+          ThumbnailUrl = "image/blogs/maui/step-by-step-setup-guide-for-dotnet-maui-with-visual-studio-code.webp",
           ContentUrl = "blogs/step-by-step-setup-guide-for-dotnet-maui-with-visual-studio-code",
-          IconUrl = "image/icons/msbuild.webp",
+          IconUrl = "image/icons/maui.webp",
           Channel = "MAUI",
           Type = "blogs",
           CreatedOn = new DateTime(2025, 3, 2, 22, 30, 0, DateTimeKind.Utc),
           ModifiedOn = new DateTime(2025, 3, 2, 22, 30, 0, DateTimeKind.Utc),
-          Keywords = ["Mac OS", "Windows", "Visual Studio Code"]
+          Keywords = ["Mac OS", "Windows", "Linux", "Visual Studio Code"]
         }
     ];
   }
