ff3197ffe35d9f9e8e38e0eb3e8f4c7459c93338
Abdul Rahman <abdulrahman.smsi@gmail.com>
2025-03-17 14:44:03 +0530

    Completed Testing Anti Pattern - Testing Private Methods

--- SharedModels/TestingLearningPath.cs
+++ SharedModels/TestingLearningPath.cs
@@ -2,7 +2,7 @@
 
 public class TestingLearningPath
 {
-  public readonly List<ContentMetaData> FullContents = new(1);
+  public readonly List<ContentMetaData> FullContents = new(2);
 
   public TestingLearningPath()
   {
@@ -23,6 +23,23 @@ public class TestingLearningPath
           Type = "blogs",
           CreatedOn = new DateTime(2025, 3, 16, 22, 30, 0, DateTimeKind.Utc),
           ModifiedOn = new DateTime(2025, 3, 16, 22, 30, 0, DateTimeKind.Utc),
+          Keywords = [ "Anti Pattern", "Unit Testing" ]
+        },
+      new ContentMetaData
+        {
+          Order = 2,
+          Title = "Unit Testing Anti-Pattern: Testing Private Methods",
+          Description = "In this post I will teach you how to avoid common unit testing anti pattern - testing private methods. All with live working demo.",
+          Author = "Abdul Rahman",
+          Slug = "unit-testing-anti-pattern-testing-private-methods",
+          PosterUrl = "image/blogs/testing/unit-testing-anti-pattern-testing-private-methods.webp",
+          ThumbnailUrl = "image/blogs/testing/unit-testing-anti-pattern-testing-private-methods.webp",
+          ContentUrl = "blogs/unit-testing-anti-pattern-testing-private-methods",
+          IconUrl = "image/icons/testing.webp",
+          Channel = "Testing",
+          Type = "blogs",
+          CreatedOn = new DateTime(2025, 3, 24, 23, 30, 0, DateTimeKind.Utc),
+          ModifiedOn = new DateTime(2025, 3, 24, 23, 30, 0, DateTimeKind.Utc),
           Keywords = [ "Anti Pattern", "Unit Testing" ]
         }
     ];
