1f7385b95f2c0d582eca55002e30db5618f7622c
Abdul Rahman <abdulrahman.smsi@gmail.com>
2025-04-15 18:51:12 +0530

    Add CodeSnippet Component with Highlightjs

---
+++ BaseComponents/CodeSnippet.razor.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SharedModels;
+
+namespace BaseComponents;
+
+public class CodeSnippetBase : ComponentBase, IAsyncDisposable
+{
+  private IJSObjectReference? module;
+
+  protected string Id = Guid.NewGuid().ToString();
+
+  [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+
+  [Parameter, EditorRequired] public string CssClass { get; set; } = default!;
+  [Parameter, EditorRequired] public RenderFragment ChildContent { get; set; } = default!;
+
+  protected override async Task OnAfterRenderAsync(bool firstRender)
+  {
+    if (firstRender)
+    {
+      module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./highlight.js");
+
+      await module.InvokeVoidAsync("highlightCode", Id);
+
+      StateHasChanged();
+    }
+  }
+
+  async ValueTask IAsyncDisposable.DisposeAsync()
+  {
+    if (module is not null)
+    {
+      await module.DisposeAsync();
+    }
+  }
+}
