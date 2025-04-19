f8e44335893ba2276061f4715b0a70ab5d38a19e
Abdul Rahman <abdulrahman.smsi@gmail.com>
2025-04-19 20:13:00 +0530

    Completed Image Recognition using ML.NET with ONNX Runtime

--- SharedModels/MLNETLearningPath.cs
+++ SharedModels/MLNETLearningPath.cs
@@ -1,7 +1,7 @@
 namespace SharedModels;
 public class MLNETLearningPath
 {
-    public readonly List<ContentMetaData> FullContents = [];
+    public readonly List<ContentMetaData> FullContents = new(2);
     public MLNETLearningPath()
     {
         FullContents = 
@@ -22,6 +22,23 @@ public class MLNETLearningPath
                 CreatedOn = new DateTime(2025, 3, 9, 22, 30, 0, DateTimeKind.Utc),
                 ModifiedOn = new DateTime(2025, 3, 9, 22, 30, 0, DateTimeKind.Utc),
                 Keywords = ["Auto ML", "Machine Learning", "Language Detection", "AI", "Artificail Intelligence"]
+            },
+            new ContentMetaData
+            {
+                Order = 2,
+                Title = "AI Powered Image Recognition in .NET with ML.NET and ONNX Runtime",
+                Description = "In this post I will teach you recognise image using ML.NET and onnx runtime in dotnet apps. All with live working demo.",
+                Author = "Abdul Rahman",
+                Slug = "ai-powered-image-recognition-in-net-with-mlnet-and-onnx-runtime",
+                PosterUrl = "image/blogs/mlnet/ai-powered-image-recognition-in-net-with-mlnet-and-onnx-runtime.webp",
+                ThumbnailUrl = "image/blogs/mlnet/ai-powered-image-recognition-in-net-with-mlnet-and-onnx-runtime.webp",
+                ContentUrl = "blogs/ai-powered-image-recognition-in-net-with-mlnet-and-onnx-runtime",
+                IconUrl = "image/icons/mlnet.webp",
+                Channel = "MLNET",
+                Type = "blogs",
+                CreatedOn = new DateTime(2025, 5, 11, 22, 30, 0, DateTimeKind.Utc),
+                ModifiedOn = new DateTime(2025, 5, 11, 22, 30, 0, DateTimeKind.Utc),
+                Keywords = ["ONNX", "Machine Learning", "Image Recognition", "AI", "Artificail Intelligence"]
             }
         ];
     }
--- SharedModels/TableOfContents.cs
+++ SharedModels/TableOfContents.cs
@@ -3,7 +3,7 @@
 public class TableOfContents
 {
   public const int PageSize = 6;
-  private readonly List<ContentMetaData> FullContents = new(133);
+  private readonly List<ContentMetaData> FullContents = new(134);
   private readonly List<AuthorMetaData> FullAuthors = new(1);
   private readonly List<SponsorMetaData> FullSponsors = new(1);
 
