df0f5ea23681dd38a1fe7b6390cbf569cad8e187
fingers10 <abdulrahman.smsi@gmail.com>
2026-09-16 20:14:17 +0530

    SEO Sitemap fix

--- SharedModels/Sitemaps.cs
+++ SharedModels/Sitemaps.cs
@@ -3,28 +3,40 @@
 public class Sitemaps
 {
   public IReadOnlyList<string> Files => [
-      "sitemap.xml",
       "sitemap-authors.xml",
+      "sitemap-blog-ai.xml",
       "sitemap-blog-blazor-wasm.xml",
+      "sitemap-blog-caching.xml",
+      "sitemap-blog-database.xml",
+      "sitemap-blog-ddd.xml",
       "sitemap-blog-dependency-injection.xml",
       "sitemap-blog-design-pattern.xml",
       "sitemap-blog-http-client.xml",
+      "sitemap-blog-json.xml",
       "sitemap-blog-linq.xml",
+      "sitemap-blog-logging.xml",
       "sitemap-blog-maui.xml",
+      "sitemap-blog-mcp.xml",
+      "sitemap-blog-memory.xml",
       "sitemap-blog-middleware.xml",
       "sitemap-blog-mlnet.xml",
       "sitemap-blog-msbuild.xml",
+      "sitemap-blog-nuget.xml",
       "sitemap-blog-oops.xml",
       "sitemap-blog-owasp.xml",
       "sitemap-blog-python.xml",
       "sitemap-blog-regex.xml",
       "sitemap-blog-report.xml",
+      "sitemap-blog-security.xml",
       "sitemap-blog-signalr.xml",
       "sitemap-blog-solid.xml",
       "sitemap-blog-tdd.xml",
       "sitemap-blog-testing.xml",
       "sitemap-blog-webapi.xml",
       "sitemap-channels.xml",
+      "sitemap-static.xml",
       "sitemap-talk.xml"
   ];
+
+      public string IndexFile => "sitemap.xml";
 }
